74dd82d2955887c0f60b2a42420f4d61ec84589c
Lakshman <ilakshmangope@gmail.com>
2020-12-05 22:18:51 +0600

    Create globalFunc.vue

---
+++ globalFunc.vue
@@ -0,0 +1,23 @@
+// main file
+
+// global function
+
+const plugin = {
+  install() {
+    // global alert message
+    Vue.prototype.showActionMessage = (type, msg) => {
+      do something....
+    };
+  
+  };
+};
+
+Vue.use(plugin);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
